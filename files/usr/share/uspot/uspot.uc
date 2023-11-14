f00b4r0/uspot

commit 7a01b0e67c0822c8e41e75608a05fe4e9f830734

uspot: client_get(): make "address" arg optional
Allows getting all clients data for a given uspot.

Signed-off-by: Thibaut VARÈNE <hacks@slashdirt.org>

files/usr/share/uspot/uspot.uc
@@ -863,43 +863,54 @@ function run_service() {
 		},
 		client_get: {
 			call: function(req) {
+				function client_get_data(client) {
+					let data = {
+						... client.data || {},
+						duration: time() - client.connect,
+					};
+
+					let timeout = +client.session;
+					if (timeout) {
+						data.seconds_remaining = timeout - data.duration;
+						// if timeout is exceeded, immediately kick client for consistency's sake
+						if (data.seconds_remaining <= 0) {
+							radius_terminate(uspot, address, radtc_sessionto);
+							client_reset(uspot, address, 'session timeout');
+							return {};
+						}
+					}
+
+					return data;
+				}
+
 				let uspot = req.args.uspot;
 				let address = req.args.address;
 
-				if (!uspot || !address)
+				if (!uspot)
 					return ubus.STATUS_INVALID_ARGUMENT;
 				if (!(uspot in uspots))
 					return ubus.STATUS_INVALID_ARGUMENT;
 
-				address = uc(address);
+				if (address) {
+					address = uc(address);
 
-				if (!uspots[uspot].clients[address])
-					return {};
-
-				let client = uspots[uspot].clients[address];
-
-				let data = {
-					... client.data || {},
-					duration: time() - client.connect,
-				};
-
-				let timeout = +client.session;
-				if (timeout) {
-					data.seconds_remaining = timeout - data.duration;
-					// if timeout is exceeded, immediately kick client for consistency's sake
-					if (data.seconds_remaining <= 0) {
-						radius_terminate(uspot, address, radtc_sessionto);
-						client_reset(uspot, address, 'session timeout');
+					if (!uspots[uspot].clients[address])
 						return {};
-					}
-				}
 
-				return data;
+					let client = uspots[uspot].clients[address];
+					return client_get_data(client);
+				}
+				else {
+					let payload = {};
+					for (let mac, client in uspots[uspot].clients)
+						payload[mac] = client_get_data(client);
+					return payload;
+				}
 			},
 			/*
-			 Get a client public state.
+			 Get a/all client public state.
 			 @param uspot: REQUIRED: target uspot
-			 @param address: REQUIRED: client MAC address
+			 @param address: OPTIONAL: client MAC address
 			 */
 			args: {
 				uspot:"",

commit 02b4e11d9a445b4e90e1c34808586f1ceae0cabc

uspot: fix forced client expiry in client_get
Fixes: 7a01b0e

Signed-off-by: Thibaut VARÈNE <hacks@slashdirt.org>

files/usr/share/uspot/uspot.uc
@@ -863,7 +863,7 @@ function run_service() {
 		},
 		client_get: {
 			call: function(req) {
-				function client_get_data(client) {
+				function client_get_data(client, uspot, address) {
 					let data = {
 						... client.data || {},
 						duration: time() - client.connect,
@@ -898,12 +898,12 @@ function run_service() {
 						return {};
 
 					let client = uspots[uspot].clients[address];
-					return client_get_data(client);
+					return client_get_data(client, uspot, address);
 				}
 				else {
 					let payload = {};
 					for (let mac, client in uspots[uspot].clients)
-						payload[mac] = client_get_data(client);
+						payload[mac] = client_get_data(client, uspot, mac);
 					return payload;
 				}
 			},
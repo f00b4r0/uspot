f00b4r0/uspot

commit 452d10040e26850f3198ccf2f50ddd23999edb49

move credentials auth to backend
Signed-off-by: Thibaut VARÈNE <hacks@slashdirt.org>

files/usr/share/uspot/uspot.uc
@@ -18,6 +18,7 @@ let uciload = uci.foreach('uspot', 'uspot', (d) => {
 		interfaces[d[".name"]] = {
 		settings: {
 			accounting,
+			auth_mode: d.auth_mode,
 			auth_server: d.auth_server,
 			auth_secret: d.auth_secret,
 			auth_port: d.auth_port || 1812,
@@ -524,6 +525,37 @@ function run_service() {
 					return { 'access-accept': 0 };
 
 				let settings = interfaces[interface].settings;
+				address = uc(address);	// spotfilter uses ether_ntoa() which is uppercase
+
+				// prepare client payload
+				let payload = {
+					data: {
+						sessionid,
+						username,	// XXX does anything use this? comes from handler.uc radius & credentials auth
+						client_ip,	// not used, could be useful
+					},
+				};
+				if (ssid)
+					payload.data.ssid = ssid;
+
+				// credentials
+				if (settings.auth_mode == 'credentials') {
+					let match = false;
+					uci.foreach('uspot', 'credentials', (d) => {
+						// check if the credentials are valid
+						if (d.interface != interface)
+							return;
+						if (d.username == username && d.password == password)
+							match = true;
+					});
+					if (match)
+						client_create(interface, address, payload);
+					return { 'access-accept': match };
+				}
+
+				// else, radius/uam - give up early if no server set
+				if (!settings.auth_server)
+					return { 'access-accept': 0 };
 
 				if (!username && !password) {
 					if  (!+settings.mac_auth)	// don't try mac-auth if not allowed
@@ -533,7 +565,6 @@ function run_service() {
 				}
 
 				let fmac = format_mac(interface, address);
-				address = uc(address);	// spotfilter uses ether_ntoa() which is uppercase
 
 				let request = {
 					username,
@@ -562,17 +593,7 @@ function run_service() {
 
 				if (radius['access-accept']) {
 					delete request.server;	// don't publish RADIUS server secret
-
-					let payload = {
-						data: {
-							sessionid: acct_session,
-							username,	// XXX does anything use this? comes from handler.uc radius & credentials auth
-						},
-						radius: { reply: radius.reply, request }	// save RADIUS payload for later use
-					};
-					if (ssid)
-						payload.data.ssid = ssid;
-
+					payload.radius = { reply: radius.reply, request };	// save RADIUS payload for later use
 					client_create(interface, address, payload);
 				}
 
@@ -592,7 +613,7 @@ function run_service() {
 
 			 operation:
 			  - call with (interface, address, client_ip) -> RADIUS MAC authentication
-			  - call with (interface, address, client_ip, username, password) -> RADIUS password auth
+			  - call with (interface, address, client_ip, username, password) -> credentials or RADIUS password auth
 			  - call with (interface, address, client_ip, username, password, challenge) -> RADIUS CHAP challenge auth
 			 */
 			args: {

files/usr/share/uspot/handler.uc
@@ -74,17 +74,8 @@ function request_credentials(ctx) {
 	}
 
 	// check if the credentials are valid
-	for (let k in portal.config) {
-		let cred = portal.config[k];
-
-		if (cred['.type'] != 'credentials')
-			continue;
-		if (cred.interface != ctx.spotfilter)
-			continue;
-		if (ctx.form_data.username != cred.username ||
-		    ctx.form_data.password != cred.password)
-			continue;
-
+	let auth = portal.uspot_auth(ctx, ctx.form_data.username, ctx.form_data.password);
+	if (auth && auth['access-accept']) {
 		portal.allow_client(ctx);
 		return;
 	}

commit 7d2ad84cbb07e5e93853f710acf3ae7d52f54fd8

uspot: make client_auth return message consistent
Fixes: 452d100

Signed-off-by: Thibaut VARÈNE <hacks@slashdirt.org>

files/usr/share/uspot/uspot.uc
@@ -712,13 +712,13 @@ function run_service() {
 
 				// credentials
 				if (settings.auth_mode == 'credentials') {
-					let match = false;
+					let match = 0;
 					uci.foreach('uspot', 'credentials', (d) => {
 						// check if the credentials are valid
 						if (d.uspot != uspot)
 							return;
 						if (d.username == username && d.password == password)
-							match = true;
+							match = 1;
 					});
 					if (match)
 						client_create(uspot, address, payload);
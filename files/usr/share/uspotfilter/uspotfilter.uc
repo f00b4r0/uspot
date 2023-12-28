f00b4r0/uspot

commit 154c98e0b77bdd4d1df73c860a2005e3c2d963d4

uspotfilter: mark client as active when set()
Clients may be set() with an old idle_since timestamp resulting in their
possible immediate removal in uspot due to the stale client timer.

Signed-off-by: Thibaut VARÈNE <hacks@slashdirt.org>

files/usr/share/uspotfilter/uspotfilter.uc
@@ -310,6 +310,8 @@ function run_service() {
 				data,
 			};
 
+			delete client.idle_since;	// clear up leftover idle time
+
 			uspots[uspot].clients[address] = client;
 
 			client_state(uspot, address, state);

commit 76003917c20565cd92c9f3cdfdfc90475ca86432

uspotfilter: client_set() only clear idle when allowing
Fixes: 154c98e

Signed-off-by: Thibaut VARÈNE <hacks@slashdirt.org>

files/usr/share/uspotfilter/uspotfilter.uc
@@ -327,7 +327,8 @@ function run_service() {
 				data,
 			};
 
-			delete client.idle_since;	// clear up leftover idle time
+			if (state)
+				delete client.idle_since;	// clear up leftover idle time
 
 			uspots[uspot].clients[address] = client;
 
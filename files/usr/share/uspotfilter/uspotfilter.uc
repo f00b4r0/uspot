f00b4r0/uspot

commit 8dcb03a37a77216d325b481cc8ba606d09bd6b3f

uspotfilter: rework neigh management
Linux may aggressively delete neighbours that are not necessarily FAILED,
in particular it may delete some STALE neighs, only to reach them again
later. This is particularly true with the default gc_neighN values which
are fairly low for a busy router.

This commit works around this by deleting clients when they have been
marked as FAILED or their FAILED neigh has been deleted. All other
deleted neigh states will result in marking the client idle if it was
allowed (which will then be purged by uspot as needed) or deleting if not.

Signed-off-by: Thibaut VARÈNE <hacks@slashdirt.org>

files/usr/share/uspotfilter/uspotfilter.uc
@@ -172,7 +172,7 @@ function rtnl_neigh_cb(msg)
 
 	let state = msg.state;
 
-	function del_neigh()
+	function lost_neigh()
 	{
 		if (neigh) {
 			client = uspots[uspot].clients[neigh];
@@ -186,14 +186,28 @@ function rtnl_neigh_cb(msg)
 				if (!client.ip4addr && !client.ip6addr)
 					client_remove(uspot, neigh);
 			}
-			// we expect to eventually receive FAILED/INCOMPLETE or DELNEIGH messages for all added neighs
-			delete uspots[uspot].neighs[dst];
 		}
 	}
 
 	// new and updated neighs are cmd RTM_NEWNEIGH. Deleted neighs are RTM_DELNEIGH
-	if (rtnl.const.RTM_DELNEIGH == cmd)
-		del_neigh();
+	if (rtnl.const.RTM_DELNEIGH == cmd) {
+		switch (state) {
+			case rtnl.const.NUD_FAILED:
+				lost_neigh();
+				break;
+			default:
+				// NUD_STALE etc: mark allowed clients as idle, delete others.
+				// Linux may aggressively delete neighs to make room even though they are still around
+				// allowed idle clients will eventually be purged by uspot - idle_since can only be cleared when neigh is (re)set
+				if (client && !client.idle_since && client.state)
+					client.idle_since = time();
+				else
+					lost_neigh();
+				break;
+		}
+		// we expect to eventually receive DELNEIGH messages for all added neighs
+		delete uspots[uspot].neighs[dst];
+	}
 	else {	// RTM_NEWNEIGH
 		// process REACHABLE / STALE / FAILED neighbour states
 		switch (state) {
@@ -219,7 +233,8 @@ function rtnl_neigh_cb(msg)
 				break;
 			case rtnl.const.NUD_FAILED:
 				// lladdr is no longer available in these states
-				del_neigh();
+				lost_neigh();
+				delete uspots[uspot].neighs[dst];
 				break;
 		}
 	}

commit 297b7857c1e0e7390d4fc19c5086a722ddf076ba

uspotfilter: fix DELNEIGH processing
The evaluation order of the compound test was incorrect, resulting in
leftover non-allowed stale clients. The test is thus fixed to ONLY allow
a client to avoid deletion IFF it is currently allowed by uspot.

Fixes: 8dcb03a

Signed-off-by: Thibaut VARÈNE <hacks@slashdirt.org>

files/usr/share/uspotfilter/uspotfilter.uc
@@ -199,8 +199,11 @@ function rtnl_neigh_cb(msg)
 				// NUD_STALE etc: mark allowed clients as idle, delete others.
 				// Linux may aggressively delete neighs to make room even though they are still around
 				// allowed idle clients will eventually be purged by uspot - idle_since can only be cleared when neigh is (re)set
-				if (client && !client.idle_since && client.state)
-					client.idle_since = time();
+				// WARNING: thus the ONLY case where a client may avoid deletion here is IFF it is allowed by uspot
+				if (+client?.state) {
+					if (!client.idle_since)
+						client.idle_since = time();
+				}
 				else
 					lost_neigh();
 				break;
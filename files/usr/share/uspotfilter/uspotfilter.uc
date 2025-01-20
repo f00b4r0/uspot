f00b4r0/uspot

commit ef0f5291365b3a2afebcd964f0eb6ecd136572fa

uspot/uspotfilter: implement disconnect_delay
Provide a setting to allow a grace period for disconnected clients,
whereby disconnection will only be acknowledged after a set timeout.
Clients returning (by being registered as network neighbours by the
kernel) within the timeout will not need to re-authenticate.

Signed-off-by: Thibaut VARÈNE <hacks@slashdirt.org>

files/usr/share/uspotfilter/uspotfilter.uc
@@ -37,6 +37,7 @@ let uciload = uci.foreach('uspot', 'uspot', (d) => {
 			setname: d.setname,
 			device,
 			debug: d.debug,
+			disconnect_delay: d.disconnect_delay,
 		},
 		clients: {},
 		neighs: {},
@@ -171,8 +172,18 @@ function rtnl_neigh_cb(msg)
 	{
 		if (neigh) {
 			client = uspots[uspot].clients[neigh];
-			if (client && (dst == client.ip4addr))
-				client_remove(uspot, neigh);
+			if (!client)
+				return;
+
+			// if a disconnect delay is set, allow a grace period where client actual removal is handled by uspot
+			if (+uspots[uspot].settings.disconnect_delay) {
+				client.discon_since ??= time();
+				delete client.idle_since;
+			}
+			else {
+				if (dst == client.ip4addr)
+					client_remove(uspot, neigh);
+			}
 		}
 	}
 
@@ -203,6 +214,7 @@ function rtnl_neigh_cb(msg)
 				uspots[uspot].neighs[dst] = mac;
 				if (client) {
 					delete client.idle_since;
+					delete client.discon_since;
 					client.ip4addr = dst;
 				}
 				else {
@@ -306,8 +318,10 @@ function run_service() {
 				data,
 			};
 
-			if (state)
+			if (state) {
 				delete client.idle_since;	// clear up leftover idle time
+				delete client.discon_since;
+			}
 
 			uspots[uspot].clients[address] = client;
 

files/etc/config/uspot
@@ -10,6 +10,7 @@
 #	option auth_mode ''		# one of 'uam', 'radius', 'credentials', 'click-to-continue'
 #	option idle_timeout '600'	# client is kicked when idle for more than N seconds, defaults to 600, option used if not provided by radius
 #	option session_timeout '0'	# client is kicked if connected for more than N seconds, defaults to 0, option used if not provided by radius
+#	option disconnect_delay '0'	# if set, a disconnected client will only be actually kicked after disconnect_delay seconds
 #	option interface ''		# network interface (from config/network) on which captive clients will be managed
 #	option setname ''		# firewall ipset name for client management
 #	option debug '0'		# turn on debugging output in logs

files/usr/share/uspot/uspot.uc
@@ -47,6 +47,7 @@ let uciload = uci.foreach('uspot', 'uspot', (d) => {
 			location_name: d.location_name,
 			idle_timeout: d.idle_timeout || 600,
 			session_timeout: d.session_timeout || 0,
+			disconnect_delay: d.disconnect_delay,
 			debug: d.debug,
 		},
 		clients: {},
@@ -515,6 +516,7 @@ function accounting(uspot) {
 	let list = uconn.call('uspotfilter', 'client_list', { interface: uspot });
 	let t = time();
 	let accounting = uspots[uspot].settings.accounting;
+	let disconnect_delay = uspots[uspot].settings.disconnect_delay;
 
 	if (!list) {
 		WARN(`${uspot} no client list from uspotfilter!`);
@@ -532,6 +534,12 @@ function accounting(uspot) {
 		if (!list[mac] || !list[mac].state) {
 			radius_terminate(uspot, mac, radtc_lostcarrier);
 			client_remove(uspot, mac, 'disconnect event');
+			continue;
+		}
+
+		if (+disconnect_delay && (+list[mac].discon_since && (t - list[mac].discon_since > +disconnect_delay))) {
+			radius_terminate(uspot, mac, radtc_lostcarrier);
+			client_remove(uspot, mac, 'delayed disconnect event');
 			continue;
 		}
 

commit 644fd6f52a8e7b1a72e0937ad0d4cea8e86186a9

uspotfilter: apply disconnect delay to active clients only
Without this fix, lingering clients would accumulate in uspotfilter.

Fixes: ef0f529

files/usr/share/uspotfilter/uspotfilter.uc
@@ -175,7 +175,7 @@ function rtnl_neigh_cb(msg)
 				return;
 
 			// if a disconnect delay is set, allow a grace period where client actual removal is handled by uspot
-			if (+uspots[uspot].settings.disconnect_delay) {
+			if (+uspots[uspot].settings.disconnect_delay && +client?.state) {
 				client.lost_since ??= time();
 				delete client.idle_since;
 			}